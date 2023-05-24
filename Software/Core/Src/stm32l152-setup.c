096b166fc3057104c9aba13ea9b8b489d71d3fd1
super-cow-powers <dm1306@york.ac.uk>
2023-05-24 21:30:20 +0100

    update

--- Software/Core/Src/stm32l152-setup.c
+++ Software/Core/Src/stm32l152-setup.c
@@ -71,23 +71,48 @@ static void _configureDacTimer(void) {
   RCC->APB1RSTR |= (RCC_APB1RSTR_TIM7RST);
   RCC->APB1RSTR &= ~(RCC_APB1RSTR_TIM7RST);
   RCC->APB1ENR |= (RCC_APB1ENR_TIM7EN);
-  TIM7->PSC = 31; //32MHz/(31+1) for 1MHz input gives 15Hz minimum output
+  TIM7->PSC = 0; //32MHz/(31+1) for 1MHz input gives 15Hz minimum output
   TIM7->CR1 &= ~(TIM_CR1_ARPE | TIM_CR1_OPM | TIM_CR1_UDIS); //Auto-reload preload, multi-pulse mode, enable Update Event
   TIM7->CR1 |= 0b1 << TIM_CR1_URS;
   TIM7->DIER |= TIM_DIER_UDE_Msk; //Enable DMA Request on update
-  TIM7->ARR = 100; //Start with Auto-reloading 2^16 -1
+  TIM7->ARR = 1000; //Start with Auto-reloading 2^16 -1
 
   DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_TIM7_STOP; //Freeze on breakpoint
 }
 
-void configure_timers() { _configureDacTimer(); }
+static void _configurePitchTimer(void) {
+  //Timer 2
+  RCC->APB1RSTR |= (RCC_APB1RSTR_TIM2RST);
+  RCC->APB1RSTR &= ~(RCC_APB1RSTR_TIM2RST);
+  RCC->APB1ENR |= (RCC_APB1ENR_TIM2EN);
+  TIM2->CR2 |= 0b1U << TIM_CR2_CCDS_Pos; //DMA on update event
+  TIM2->SMCR |= 0b100U << TIM_SMCR_SMS_Pos; //Slave Reset Mode
+  TIM2->DIER |= 1U << TIM_DIER_UDE_Pos; // DMA On Update
+  TIM2->CCR1 |= 0b10 << 0; // CC1S to TI2
+  
+  //TIM2->CR1 |= 
+}
+
+static void _configureVolumeTimer(void) {
+  //Timer 3
+  RCC->APB1RSTR |= (RCC_APB1RSTR_TIM3RST);
+  RCC->APB1RSTR &= ~(RCC_APB1RSTR_TIM3RST);
+  RCC->APB1ENR |= (RCC_APB1ENR_TIM3EN);
+  
+}
+
+void configure_timers() {
+  _configureDacTimer();
+  _configurePitchTimer();
+  _configureVolumeTimer();
+}
 
 void configure_dac() {
   RCC->APB1RSTR |= RCC_APB1RSTR_DACRST;
   RCC->APB1RSTR &= ~RCC_APB1RSTR_DACRST;
   RCC->APB1ENR |= RCC_APB1ENR_DACEN;
   DAC->DHR12R1 = 2000;
-  DAC->CR |= DAC_CR_BOFF1 | DAC_CR_EN1; //Enable output buffer, enable DAC
+  DAC->CR |=   DAC_CR_BOFF1 |DAC_CR_EN1; //No output buffer, enable DAC
 }
 
 void start_output() {
@@ -106,6 +131,11 @@ void configure_gpio (void) {
     | (0b00 << (CAL_PIN*2)) | (0b11 << (DAC_OUT_PIN*2));
   //Pull-up Cal button pin
   GPIOA->PUPDR |= (0b01 << (CAL_PIN*2));
+
+  //Set AFs
+  GPIOA->AFR[0] |= 1U >> (PITCH_PIN * 4);
+  GPIOA->AFR[0] |= 0b11U >> (VOLUME_PIN * 4);
+  
 }
 
 /** @brief Configures the NVIC with the used IRQs.
